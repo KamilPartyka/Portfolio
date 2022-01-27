5ba16d556787f05925c0b756d69e87c4121d349c
KamilPartyka <kamilkrzysztofpartyka@gmail.com>
2022-01-27 17:07:18 +0100

    fixing typos

--- src/components/sections/Contact/Contact.js
+++ src/components/sections/Contact/Contact.js
@@ -50,6 +50,7 @@ const Contact = () => {
     let isFormValid = true;
     const errors = {};
 
+    // eslint-disable-next-line prefer-regex-literals
     const pattern = new RegExp(
       // eslint-disable-next-line no-useless-escape
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
