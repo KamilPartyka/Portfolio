906913aa71aeba3e1e0fe5328e8e38d94c2f2270
KamilPartyka <kamilkrzysztofpartyka@gmail.com>
2025-04-05 12:02:57 +0200

    fix: adjust Hero section height to improve layout

--- src/sections/Hero/Hero.js
+++ src/sections/Hero/Hero.js
@@ -25,7 +25,7 @@ const Hero = ({ reference }) => (
       style={{
         position: 'absolute',
         width: '100%',
-        height: '100%',
+        height: '110%',
         zIndex: -1,
       }}
     />
